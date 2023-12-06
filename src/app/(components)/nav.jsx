e8367fe1bb38d1790b3a794b8ea4adc06fac61cd
rnfe <86198119+ngoctran1401@users.noreply.github.com>
2023-12-06 08:53:01 +0700

    Update navbar-fe

--- src/app/(components)/nav.jsx
+++ src/app/(components)/nav.jsx
@@ -1,98 +1,128 @@
 import Link from "next/link";
-import Image from "next/image";
+import "../css/header.css";
+import "../css/bootstrap.min.css";
 const Nav = () => {
   return (
-    <header className="flex-col">
-      <div className="flex justify-center py-1 bg-page">
-        <p className="text-xs 2xl:text-xl font-semibold">Tham gia</p>
+    <header>
+      {/* <!-- Google Web Fonts --> */}
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Playball&display=swap"
+        rel="stylesheet"
+      />
+
+      {/* <!-- Icon Font Stylesheet --> */}
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+      />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
+        rel="stylesheet"
+      />
+
+      {/* <!-- Customized Bootstrap Stylesheet --> */}
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+      />
+
+      {/* <!-- Template Stylesheet --> */}
+      <link href="css/bootstrap.min.css" rel="stylesheet" />
+      <link href="css/header.css" rel="stylesheet"></link>
+      <div class="flex justify-center py-1 bg-page">
+        <p class="text-sm 2xl:text-xl font-semibold">Tham gia</p>
         <Link
           href="/"
-          className="text-xs 2xl:text-xl cursor-pointer font-semibold text-yellow"
+          class="text-sm 2xl:text-xl cursor-pointer font-semibold text-yellow"
         >
           &nbsp;hội viên&nbsp;
         </Link>
-        <p className="text-xs 2xl:text-xl font-semibold">ngay hôm nay!!!</p>
+        <p class="text-sm 2xl:text-xl font-semibold">ngay hôm nay!!!</p>
       </div>
-      <div className="flex flex-row py-3 2xl:py-4 items-center bg-nav">
-        <div className="flex-none w-20 ml-20 cursor-pointer 2xl:w-40">
-          <Link href="/" className="">
-            <Image
-              href="/"
-              src="/logo2.svg"
-              width={100}
-              height={100}
-              alt="Logo"
-            />
-          </Link>
-        </div>
-        <nav className="grow">
-          <ul className="flex justify-center gap-[5vw] ">
-            <li>
-              <Link href="/" className="text-base	text-black-text 2xl:text-xl">
-                Phim Mới
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/review/1"
-                className="text-base text-black-text 2xl:text-xl"
+
+      
+
+      <div class="container-fluid nav-bar">
+        <div class="container">
+          <nav class="navbar navbar-light navbar-expand-lg py-4">
+            <a href="index.html" class="navbar-brand">
+              <h1 class="text-primary fw-bold mb-0">
+                Cater<span class="text-dark">Serv</span>
+              </h1>
+            </a>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+              <div class="navbar-nav mx-auto">
+                <a href="" class="nav-item nav-link active">
+                  Home
+                </a>
+                <a href="about.html" class="nav-item nav-link">
+                  About
+                </a>
+                <a href="service.html" class="nav-item nav-link">
+                  Services
+                </a>
+                <a href="event.html" class="nav-item nav-link">
+                  Events
+                </a>
+                <a href="menu.html" class="nav-item nav-link">
+                  Menu
+                </a>
+                <div class="nav-item dropdown">
+                  <a
+                    href="#"
+                    class="nav-link dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                  >
+                    Pages
+                  </a>
+                  <div class="dropdown-menu bg-light">
+                    <a href="book.html" class="dropdown-item">
+                      Booking
+                    </a>
+                    <a href="blog.html" class="dropdown-item">
+                      Our Blog
+                    </a>
+                    <a href="team.html" class="dropdown-item">
+                      Our Team
+                    </a>
+                    <a href="testimonial.html" class="dropdown-item">
+                      Testimonial
+                    </a>
+                    <a href="404.html" class="dropdown-item">
+                      404 Page
+                    </a>
+                  </div>
+                </div>
+                <a href="contact.html" class="nav-item nav-link">
+                  Contact
+                </a>
+              </div>
+              <button
+                class="btn-search btn btn-primary btn-md-square me-4 rounded-circle d-none d-lg-inline-flex"
+                data-bs-toggle="modal"
+                data-bs-target="#searchModal"
               >
-                Top 100
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/home"
-                className="text-base	text-black-text 2xl:text-xl"
+                <i class="fas fa-search"></i>
+              </button>
+              <a
+                href=""
+                class="btn btn-primary py-2 px-4 d-none d-xl-inline-block rounded-pill"
               >
-                Games
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/home"
-                className="text-base	text-black-text 2xl:text-xl"
-              >
-                Animes
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/home"
-                className="text-base	text-black-text 2xl:text-xl"
-              >
-                Hot Trend
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="flex flex-row w-20 flex-shrink-0 mr-20 cursor-pointer justify-center gap-[1vw] 2xl:w-40">
-          <Link href="/" className="">
-            <Image
-              src="/icon_banner/search-outline.svg"
-              width={20}
-              height={20}
-              alt="Logo"
-            ></Image>
-          </Link>
-          <Link href="/" className="">
-            <Image
-              src="/icon_banner/person-outline.svg"
-              width={20}
-              height={20}
-              alt="Logo"
-            ></Image>
-          </Link>
-          <Link href="/" className="">
-            <Image
-              src="/icon_banner/heart-outline.svg"
-              width={20}
-              height={20}
-              alt="Logo"
-            ></Image>
-          </Link>
+                Book Now
+              </a>
+            </div>
+          </nav>
         </div>
       </div>
+      {/* <!-- Navbar End -->
+              {/* <!-- JavaScript Libraries --> */}
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
+      {/* <!-- Template Javascript --> */}
+      <script src="./header.jsx"></script>
     </header>
   );
 };
